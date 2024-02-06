b278c67af419ab1902747e262aa1880068b1d3cc
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2024-02-06 16:31:24 +0200

    added boiler plate code and established a file structure.

---
+++ get_it_done/lib/features/navigation/navigation.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatelessWidget {
+  const Navigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Home Page"),
+      ),
+    );
+  }
+}
