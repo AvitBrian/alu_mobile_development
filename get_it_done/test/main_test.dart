a4d7123f704caee7c871d7025d8ab277157889cd
b.mugisha2 <b.mugisha2@alustudent.com>
2024-03-20 16:18:19 +0000

    updates

--- get_it_done/test/main_test.dart
+++ get_it_done/test/main_test.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it_done/main.dart';
 
 void main() {
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
   testWidgets('MyApp builds correctly', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
