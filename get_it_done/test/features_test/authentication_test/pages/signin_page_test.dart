d47f2351ddaf3bd044822091e040823b8c5f639d
b.mugisha2 <b.mugisha2@alustudent.com>
2024-03-20 16:55:55 +0000

    testing

--- get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
+++ get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
@@ -4,11 +4,9 @@ import 'package:get_it_done/features/authentication/pages/signin_page.dart';
 
 void main() {
   testWidgets('Sign in form UI test', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: SignInForm(),
-    ));
+    await tester.pumpWidget(SignInForm());
 
-    // expect(find.byType(TextField), findsNWidgets(2));
+    expect(find.byType(TextField), findsNWidgets(2));
     expect(find.text('Sign in'), findsOneWidget);
     expect(find.text('Continue with Google'), findsOneWidget);
   });
