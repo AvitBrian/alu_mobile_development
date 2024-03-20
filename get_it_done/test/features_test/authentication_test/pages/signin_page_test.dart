6c29dc743edaf5210479dfb3429865da53af66d4
b.mugisha2 <b.mugisha2@alustudent.com>
2024-03-20 16:58:13 +0000

    testing

--- get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
+++ get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it_done/features/authentication/pages/signin_page.dart';
+import 'package:get_it_done/providers/provider.dart';
+import 'package:get_it_done/features/authentication/widgets/signin_form.dart';
 
 void main() {
-  testWidgets('Sign in form UI test', (WidgetTester tester) async {
-    await tester.pumpWidget(SignInForm());
+  runApp(
+    MyApp(),
+  );
+}
 
-    expect(find.byType(TextField), findsNWidgets(2));
-    expect(find.text('Sign in'), findsOneWidget);
-    expect(find.text('Continue with Google'), findsOneWidget);
-  });
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ChangeNotifierProvider(
+        create: (_) => AuthStateProvider(), // Example provider creation
+        child: SignInForm(),
+      ),
+    );
+  }
 }
