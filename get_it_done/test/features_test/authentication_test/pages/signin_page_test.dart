96a3a17a810f55a8d60b301a2234fa49a0328dee
b.mugisha2 <b.mugisha2@alustudent.com>
2024-03-20 16:36:19 +0000

    updates

--- get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
+++ get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it_done/features/authentication/pages/signin_page.dart';
+import 'package:get_it_done/providers/provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   group('SignInForm methods', () {
     testWidgets('handleGoogleSignIn - Test', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: SignInForm()));
+      // Wrap SignInForm with Provider<AuthStateProvider>
+      await tester.pumpWidget(
+        Provider<AuthStateProvider>(
+          create: (_) => AuthStateProviderMock(),
+          child: MaterialApp(home: SignInForm()),
+        ),
+      );
 
+      // Trigger the handleGoogleSignIn
       await tester.tap(find.byType(ElevatedButton));
       await tester.pump();
 
+      // Verify that the handleGoogleSignIn button was tapped
       expect(find.text('Google Sign In'), findsOneWidget);
     });
 
     testWidgets('handleEmailAndPasswordSignIn - Test', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: SignInForm()));
+      // Wrap SignInForm with Provider<AuthStateProvider>
+      await tester.pumpWidget(
+        Provider<AuthStateProvider>(
+          create: (_) => AuthStateProviderMock(),
+          child: MaterialApp(home: SignInForm()),
+        ),
+      );
 
       // Trigger the handleEmailAndPasswordSignIn
       await tester.tap(find.byType(TextButton));
