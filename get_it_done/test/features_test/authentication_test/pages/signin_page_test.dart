6a65900e926a0898a838701db173b1931b9d6492
b.mugisha2 <b.mugisha2@alustudent.com>
2024-03-20 16:33:52 +0000

    updates

--- get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
+++ get_it_done/test/features_test/authentication_test/pages/signin_page_test.dart
@@ -5,23 +5,19 @@ import 'package:get_it_done/features/authentication/pages/signin_page.dart';
 void main() {
   group('SignInForm methods', () {
     testWidgets('handleGoogleSignIn - Test', (WidgetTester tester) async {
-      // Build our SignInForm widget
       await tester.pumpWidget(MaterialApp(home: SignInForm()));
 
-      // Trigger the handleGoogleSignIn
-      await tester.tap(find.text('Google Sign In'));
+      await tester.tap(find.byType(ElevatedButton));
       await tester.pump();
 
-      // Verify that the handleGoogleSignIn button was tapped
       expect(find.text('Google Sign In'), findsOneWidget);
     });
 
     testWidgets('handleEmailAndPasswordSignIn - Test', (WidgetTester tester) async {
-      // Build our SignInForm widget
       await tester.pumpWidget(MaterialApp(home: SignInForm()));
 
       // Trigger the handleEmailAndPasswordSignIn
-      await tester.tap(find.text('Email Sign In'));
+      await tester.tap(find.byType(TextButton));
       await tester.pump();
 
       // Verify that the handleEmailAndPasswordSignIn button was tapped
